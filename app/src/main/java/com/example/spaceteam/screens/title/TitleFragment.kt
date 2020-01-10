b5807eed7fee1ebffc00f7cfb1bb514046a57f88
MissKnackii <camille.chaulaic@gmail.com>
2020-01-10 16:28:55 +0100

    Commit fin de journée

--- app/src/main/java/com/example/spaceteam/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/spaceteam/screens/title/TitleFragment.kt
@@ -1,4 +1,12 @@
 package com.example.spaceteam.screens.title
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
 class TitleFragment {
+
 }
