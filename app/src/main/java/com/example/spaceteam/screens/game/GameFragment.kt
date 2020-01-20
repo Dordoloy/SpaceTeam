4e76e441a986d4bc85689c8055cc97bd85d92c61
dordoloy <yohan.dordolo@orange.com>
2020-01-20 16:47:52 +0100

    save end of day

--- app/src/main/java/com/example/spaceteam/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/spaceteam/screens/game/GameFragment.kt
@@ -1,19 +1,25 @@
 package com.example.spaceteam.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import com.example.spaceteam.Config
 import com.example.spaceteam.R
 import com.example.spaceteam.databinding.GameFragmentBinding
+import com.example.spaceteam.serviceWeb.WebSocketConnection
 
 /**
  * Fragment of game
  */
 class GameFragment: Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     /**
      * called on the creation of fragment
@@ -34,9 +40,17 @@ class GameFragment: Fragment() {
             R.layout.game_fragment, container, false
         )
 
+        WebSocketConnection().testConnection()
+        WebSocketConnection().joinRoom("myRoom",1)
+
         binding.finishButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_gameFragment_to_scoreFragment)
         }
+
+//        viewModel.event.observe(this, Observer { vent ->
+//            Log.d(Config.TAG, event.toString())
+//            observeEvent(event)
+//        })
 
         return binding.root
     }
--- app/src/main/java/com/example/spaceteam/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/spaceteam/screens/game/GameViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.spaceteam.screens.game
 
+import androidx.lifecycle.LiveData
+import com.example.spaceteam.model.Event
+import com.example.spaceteam.serviceWeb.WebSocketConnection
+
 /**
  * View of game
  */
-class GameViewModel {
+class GameViewModel(val webSocketListener: WebSocketConnection) {
+
+    fun getLastEventReceived() : LiveData<Event> = webSocketListener.lastEventReceived
+
+
 }
