861a146433a5e2c36b1ba9781f3d3fc09200606d
anthonychaussin <chaussin.anthony@gmail.com>
2020-01-20 16:51:31 +0100

    User list in score
    Signed-off-by: anthonychaussin <chaussin.anthony@gmail.com>

--- app/src/main/java/com/example/spaceteam/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/spaceteam/screens/score/ScoreViewModel.kt
@@ -1,6 +1,26 @@
 package com.example.spaceteam.screens.score
 
+import com.example.spaceteam.model.UserList
+import com.example.spaceteam.serviceWeb.SpaceTeamService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 /**
  * score view
  */
-class ScoreViewModel
+class ScoreViewModel {
+
+    fun GetUerList() {
+        SpaceTeamService.retrofit.userList().enqueue(object : Callback<UserList> {
+            override fun onFailure(call: Call<UserList>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<UserList>, response: Response<UserList>) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
+
+}
